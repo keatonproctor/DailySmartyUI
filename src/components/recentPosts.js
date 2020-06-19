f247654aa10d2aa6e02ad2aab43360ce36a3cf26
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-06-19 16:59:55 -0600

    Set up redux forum in searchbar.js

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -7,9 +7,9 @@ class SearchBar extends Component {
                 <div className="recent-posts__wrapper">
                     <div className="recent-posts__heading">Recent Posts</div>
                     <ul className="recent-posts__posts">
-                        <li>recent post 0</li>
-                        <li>recent post 1</li>
-                        <li>recent post 2</li>
+                        <li>Recent Post 0</li>
+                        <li>Recent Post 1</li>
+                        <li>Recent post 2</li>
                     </ul>
                 </div>
 
